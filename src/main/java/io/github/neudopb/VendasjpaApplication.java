809891c99e4930d90237f7cfc481d452fbb45ef4
Neudo Paulino <neudinho66@gmail.com>
2022-07-01 09:59:19 -0300

    first commit

---
+++ src/main/java/io/github/neudopb/VendasjpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.neudopb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/neudopb/VendasjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.neudopb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
