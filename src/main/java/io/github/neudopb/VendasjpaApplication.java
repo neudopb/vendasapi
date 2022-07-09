0475126c44cf2f44c40eadb9fc3fea1d7a3e3850
Neudo Paulino <neudinho66@gmail.com>
2022-07-09 20:14:08 -0300

    feat: Request GET Cliente

--- src/main/java/io/github/neudopb/VendasjpaApplication.java
+++ src/main/java/io/github/neudopb/VendasjpaApplication.java
@@ -1,10 +1,23 @@
 package io.github.neudopb;
 
+import io.github.neudopb.domain.entity.Cliente;
+import io.github.neudopb.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasjpaApplication {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ClienteRepository repository) {
+		return args -> {
+			Cliente c = new Cliente("Neudo");
+			repository.save(c);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasjpaApplication.class, args);
--- src/main/java/io/github/neudopb/api/controller/ClienteController.java
+++ src/main/java/io/github/neudopb/api/controller/ClienteController.java
@@ -1,18 +1,32 @@
 package io.github.neudopb.api.controller;
 
+import io.github.neudopb.domain.entity.Cliente;
+import io.github.neudopb.domain.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    private ClienteRepository repository;
+
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nome) {
-        return String.format("Hello %s", nome);
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = repository.findById(id);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
