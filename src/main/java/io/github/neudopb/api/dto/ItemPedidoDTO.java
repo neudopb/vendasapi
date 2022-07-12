cb7af13d647dd43191d36a34cfaf32959b0d2062
Neudo Paulino <neudinho66@gmail.com>
2022-07-12 09:35:53 -0300

    chore: Create DTO Pedido and ItemPedido

---
+++ src/main/java/io/github/neudopb/api/dto/ItemPedidoDTO.java
@@ -0,0 +1,14 @@
+package io.github.neudopb.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedidoDTO {
+
+    private Integer produto;
+    private Integer quantidade;
+}
