5740816b6ebe77da2addc975fd9e8b817297545c
Neudo Paulino <neudinho66@gmail.com>
2022-07-12 14:11:40 -0300

    feat: Controller Advice and ExceptionHandler

---
+++ src/main/java/io/github/neudopb/api/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,18 @@
+package io.github.neudopb.api.controller;
+
+import io.github.neudopb.api.ApiErrors;
+import io.github.neudopb.exception.RegraNegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        return new ApiErrors(ex.getMessage());
+    }
+}
