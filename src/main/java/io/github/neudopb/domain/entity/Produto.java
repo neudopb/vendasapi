6ab6fe52f4793d4744168f33ab3aa066c9ded5dd
Neudo Paulino <neudinho66@gmail.com>
2022-07-12 17:02:50 -0300

    feat: Validate Entity Produto

--- src/main/java/io/github/neudopb/domain/entity/Produto.java
+++ src/main/java/io/github/neudopb/domain/entity/Produto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
@@ -20,9 +22,11 @@ public class Produto {
     private Integer id;
 
     @Column(name = "descricao")
+    @NotEmpty(message = "Campo descricao é obrigatório")
     private String descricao;
 
     @Column(name = "preco_unitario")
+    @NotNull(message = "Campo preco é obrigatório")
     private BigDecimal preco;
 
 }
