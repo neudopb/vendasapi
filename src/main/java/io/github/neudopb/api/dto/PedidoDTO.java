53b07433407522ce84519cfba7c565ee79a2765a
Neudo Paulino <neudinho66@gmail.com>
2022-07-13 08:50:22 -0300

    feat: Validate Entity Pedido and annotation NotEmptyList

--- src/main/java/io/github/neudopb/api/dto/PedidoDTO.java
+++ src/main/java/io/github/neudopb/api/dto/PedidoDTO.java
@@ -1,9 +1,11 @@
 package io.github.neudopb.api.dto;
 
+import io.github.neudopb.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -12,7 +14,10 @@ import java.util.List;
 @NoArgsConstructor
 public class PedidoDTO {
 
+    @NotNull(message = "O campo código do cliente é obrigatório")
     private Integer cliente;
+    @NotNull(message = "O campo total do pedido é obrigatório")
     private BigDecimal total;
+    @NotEmptyList(message = "Pedido não pode ser realizado sem itens")
     private List<ItemPedidoDTO> itens;
 }
